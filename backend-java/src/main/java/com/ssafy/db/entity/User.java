922a865e8517e568ea01c39e330dd1662a896461
이건 <akstp88@gmail.com>
2022-01-19 16:42:29 +0900

    feat: Add signUp function

--- backend-java/src/main/java/com/ssafy/db/entity/User.java
+++ backend-java/src/main/java/com/ssafy/db/entity/User.java
@@ -1,26 +1,45 @@
 package com.ssafy.db.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ssafy.api.request.UserRegisterPostReq;
 
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-
 /**
  * 유저 모델 정의.
  */
-@Entity
+@Entity(name = "user")
 @Getter
 @Setter
 public class User extends BaseEntity{
-    String position;
-    String department;
+    String disability;
     String name;
+    String nickname;
+    String email;
+    
+    @Column(name = "user_id")
     String userId;
 
     @JsonIgnore
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     String password;
+    
+    protected User() {
+    }
+
+	public User(UserRegisterPostReq u) {
+		this.disability = u.getDisability();
+		this.name = u.getName();
+		this.nickname = u.getNickname();
+		this.email = u.getEmail();
+		this.userId = u.getUserId();
+		this.password = u.getPassword();
+	}
+    
+    
 }
