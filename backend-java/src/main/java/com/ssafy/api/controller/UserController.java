2af07b2e9aa01d7cf710bf80c6ca35efff61280c
이건 <akstp88@gmail.com>
2022-01-21 13:10:03 +0900

    feat: Add sign up validation check

--- backend-java/src/main/java/com/ssafy/api/controller/UserController.java
+++ backend-java/src/main/java/com/ssafy/api/controller/UserController.java
@@ -44,17 +44,18 @@ public class UserController {
 	@ApiOperation(value = "회원 가입", notes = "<strong>아이디와 패스워드</strong>를 통해 회원가입 한다.") 
     @ApiResponses({
         @ApiResponse(code = 200, message = "성공"),
-        @ApiResponse(code = 401, message = "인증 실패"),
-        @ApiResponse(code = 404, message = "사용자 없음"),
+        @ApiResponse(code = 400, message = "유효성 검증 실패"),
         @ApiResponse(code = 500, message = "서버 오류")
     })
 	public ResponseEntity<? extends BaseResponseBody> register(
-			@RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo) {
-		
-		//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
-		User user = userService.createUser(registerInfo);
-		
-		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+			@Valid @RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo, Errors errors) {
+		if(errors.hasErrors()) {
+			return ResponseEntity.status(400).body(BaseResponseBody.of(400, errors.getFieldError().getDefaultMessage()));
+		} else {
+			//임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
+			User user = userService.createUser(registerInfo);
+			return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+		}
 	}
 	
 	@GetMapping("/me")
--- backend-java/src/main/java/com/ssafy/api/request/UserRegisterPostReq.java
+++ backend-java/src/main/java/com/ssafy/api/request/UserRegisterPostReq.java
@@ -12,16 +12,35 @@ import lombok.Setter;
 @Setter
 @ApiModel("UserRegisterPostRequest")
 public class UserRegisterPostReq {
+
 	@ApiModelProperty(name="유저 ID", example="ssafy_web")
+    @NotBlank(message = "Id size invalid")
+    @Pattern(regexp = "[0-9|A-Z|a-z]+", message = "Id pattern invalid")
+    @Size(min = 3, max = 20, message = "Id size invalid")
 	String userId;
+    
 	@ApiModelProperty(name="유저 Password", example="your_password")
+    @NotBlank(message = "Password size invaalid")
+	@Pattern(regexp = "(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&]).+", message = "Password Pattern invalid")
+    @Size(min = 8, max = 24, message = "Password size invalid")
 	String password;
+	
 	@ApiModelProperty(name="장애 종류", example="시각장애")
+    @NotBlank(message = "disability size invaild")
+    @Size(min = 1, max = 5, message = "disability size invaild")
 	String disability;
+	
 	@ApiModelProperty(name="이름", example="홍길동")
+    @NotBlank(message = "Name size invalid")
+    @Size(min = 2, max = 5, message = "Name size invalid")
 	String name;
+	
 	@ApiModelProperty(name="닉네임", example="길동이")
+    @NotBlank(message = "Nickname size invalid")
+    @Size(min = 2, max = 10, message = "Nickname size invalid")
 	String nickname;
+	
 	@ApiModelProperty(name="이메일", example="abc@naver.com")
-	String email;
-}
+    @NotBlank(message = "Email size invalid")
+    @Size(min = 2, max = 30, message = "Email size invalid")
+	@Email(regexp = "[0-9a-zA-Z]+[@]{1}[0-9a-zA-Z]+[.]{1}[a-zA-Z]+", message = "Email Pattern invalid")
