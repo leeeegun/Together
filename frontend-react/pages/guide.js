90ed52060f19de60b52499b677e13e37335c32a4
KIM Tae Hun <gift2barry@gmail.com>
2022-02-15 14:41:43 +0900

    feat: Add wai-aria on the guide page

--- frontend-react/pages/guide.js
+++ frontend-react/pages/guide.js
@@ -25,7 +25,7 @@ function guide() {
         },
       }}
     >
-      <div className="flex text-center justify-center items-center mt-5 text-4xl font-extralight hover:font-normal">
+      <div className="flex items-center justify-center mt-5 text-4xl text-center font-extralight hover:font-normal">
         <Link href="/">
           <motion.img
             src="images/svg/Together.svg"
@@ -35,19 +35,19 @@ function guide() {
           ></motion.img>
         </Link>
       </div>
-      <div className="guide-container px-10 py-10 w-3/5 min-h-fit">
+      <div className="w-3/5 px-10 py-10 guide-container min-h-fit">
         <div id="guide" className="">
-          <p className="">
+          <p tabIndex="0" className="">
             <strong className="text-3xl">📖 가이드</strong>
           </p>
-          <p className="my-10 flex flex-col">
+          <p className="flex flex-col my-10">
             <Image
               src={RandomImage}
               alt="이미지"
               className="mx-auto rounded-3xl"
             ></Image>
           </p>
-          <p className="mt-5">
+          <p tabIndex="0" className="mt-5">
             Together는 시각 및 청각 장애인들을 위한 화상회의 플랫폼입니다.
             누구나 신체적 제약 없이 사용 할 수 있는 화상회의를 목적으로 구현된
             Together는 여러분들에게 색다른 접근성을 제시할 것입니다. 혹시 전화
@@ -58,7 +58,7 @@ function guide() {
             토대로 구현되었습니다.
           </p>
           <br></br>
-          <p className="">
+          <p tabIndex="0" className="">
             Together는 색각 이상자를 위한 각각의 구별되는 컬러톤을 사용하여
             시각적 효과를 증진하였습니다. 또한, 시각적 효과를 부각하기 위해
             애니메이션 효과로 사용자가 어떤 창에 포커스 하고있는지를 즉각적으로
@@ -66,7 +66,7 @@ function guide() {
             사용자로 하여금 최고의 경험을 선사합니다.
           </p>
           <br></br>
-          <div className="hover:cursor-zoom-in">
+          <div tabIndex="0" className="hover:cursor-zoom-in">
             <strong className="hover:text-slate-700 ">
               전맹 시각 장애인의 경우, 기존에 사용하는 TTS 리더를 사용하시거나,
               Together에서 제공하는 TTS 기능을 사용하실 수 있습니다. TTS 리더에
@@ -75,12 +75,12 @@ function guide() {
             </strong>
           </div>
           <br></br>
-          <p>
+          <p tabIndex="0">
             청각 장애인의 경우, Together가 제공하는 STT기능을 사용하여 다른 회의
             참여자가 하는 음성을 말풍선 형태의 자막으로 제공받으실 수 있습니다.
           </p>
 
-          <ul className="my-5 mx-5">
+          <ul className="mx-5 my-5">
             <CustomLi
               text="STT(음성인식 자막) 기능을 사용하려면 어떻게 해야 하죠?"
               description="음성인식 자막 기능은 Together에서 기본으로 제공하는 기능입니다. 화상 회의 내에 자동으로 생성되는 말풍선으로 자막이 제공되며, 말풍선 기능의 사용이 필요하지 않은 경우, 토글 버튼을 통해 자막 기능을 끌 수 있습니다."
