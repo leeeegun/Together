d37ed10873350810834b861423bd17165f0d1902
이호성 <4225-leeho_@users.noreply.lab.ssafy.com>
2022-02-15 19:27:55 +0900

    feat: User modify refector

--- backend-java/src/main/java/com/ssafy/api/service/UserServiceImpl.java
+++ backend-java/src/main/java/com/ssafy/api/service/UserServiceImpl.java
@@ -52,7 +52,6 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public void setUserInfoByUserId(User user, UserModifyPostReq userInfo) {
 		user.setDisability(userInfo.getDisability());
-		user.setPassword(passwordEncoder.encode(userInfo.getPassword()));
 		user.setNickname(userInfo.getNickname());
 		
 		userRepository.flush();
